8490d90f23c8080f22480545eeb360472feb47cc
Skufach <M.Skusnov@gmail.com>
2019-03-09 22:42:51 +0200

    First screen added

--- src/components/PokemonSets/PokemonSets.js
+++ src/components/PokemonSets/PokemonSets.js
@@ -6,14 +6,34 @@ export class PokemonSets extends React.Component {
 
     render() {
         const {pokemon_set} = this.props
+        const middle_section = pokemon_set.standardLegal || pokemon_set.expandedLegal
+
 
         return (
-            <li className='pokemon-set'>
-                <img className='pokemon-set_logo' src={`${pokemon_set.logoUrl}`} alt=""/>
+            <li key={pokemon_set.id} className="pokemon-set">
 
-                <img className='pokemon-set_symbol' src={`${pokemon_set.symbolUrl}`} alt=""/>
-                <h3>{pokemon_set.releaseDate}</h3>
-                {/*{pokemon_set.symbolUrl}*/}
+                <div className="pokemon-set_logo">
+                    <img src={`${pokemon_set.logoUrl}`} alt=""/>
+                </div>
+
+                <div className="pokemon-set_title">
+                    <img className='pokemon-set_symbol' src={`${pokemon_set.symbolUrl}`} alt=""/>
+
+                    <div className="pokemon-set_title-text">
+                        <h3>{pokemon_set.name}</h3>
+                        <p>Released {pokemon_set.releaseDate}</p>
+                    </div>
+
+                </div>
+
+                {middle_section ?
+                    <ul className="pokemon-set_list">
+                        {pokemon_set.standardLegal ? <li>Standard Legal</li> : null}
+                        {pokemon_set.expandedLegal ? <li>Expanded Legal</li> : null}
+
+                    </ul> : null
+                }
+
             </li>
         )
     }
